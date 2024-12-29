7346c9dcaeaf093e82894f0af0336590eb5ad144
Om Suthar <91866686+ompsuthar9000@users.noreply.github.com>
2024-12-29 20:58:35 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../server/dist'  // Ensure the output goes to the correct folder
+  },
   plugins: [react()],
 })
