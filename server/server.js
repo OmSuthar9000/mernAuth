cea3ad8b38bc2471da2c19401ab095648ef1006e
OmSuthar9000 <94785404+OmSuthar9000@users.noreply.github.com>
2024-12-29 22:21:41 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,12 +38,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use("/api/user", UserRouter);
 
 // Serve React app for any other route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.get("/", (req, res) => {
-  res.json({"message":"working"});
-});
 // Start the server
 app.listen(port, () => console.log(`App is running on port: ${port}`));
